7a0f195b7b0ea6efbb42363842037323d587824d
Rudy <justrudyrodriguez@gmail.com>
2020-12-11 17:33:40 -0800

    Added a pre-trained Pytorch model. Converting it to onnx.

---
+++ pyTorch.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+
+dummy_in = torch.randn(10, 3, 224, 224)
+model = torchvision.models.resnet18(pretrained=True)
+
+in_names = [ "actual_input_1" ] + [ "learned_%d" % i for i in range(16) ]
+out_names = [ "output1" ]
+
+torch.onnx.export(model, dummy_in, "resnet18.onnx", input_names=in_names, output_names=out_names, opset_version=7, verbose=True)
